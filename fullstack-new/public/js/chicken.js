b9ee38502733bd4b6eb43daee2410b01cbe605fc
TaikoNep <jekai.simmons2004@gmail.com>
2025-11-04 13:47:18 -0500

    Adding loading bar

--- fullstack-new/public/js/chicken.js
+++ fullstack-new/public/js/chicken.js
@@ -1,13 +1,19 @@
 console.log("Hello Chicken Lovers");
 const chickenDiv = document.getElementsByClassName("chickens")[0];
+const loadingIcon = document.getElementById("loadingIcon");
 
 fetch("/api/chickens/").then((response) => 
-    {return response.json()}).then((response) =>
+        {console.log(response);
+        loadingIcon.style.transform = "rotate(180deg)";
+        return response.json()}).then((response) =>
         {for (i in response) {
+            console.log(response);
             const chicken = response[i];
             const curChicken = document.createElement("div");
             curChicken.className = 'chicken';
             chickenDiv.append(curChicken);
             curChicken.style.backgroundColor = chicken['color'];
             curChicken.innerHTML = '<p class="name">'+chicken['name']+'</p>'
-        }});
+        }
+        loadingIcon.style.display = "none";
+    });
